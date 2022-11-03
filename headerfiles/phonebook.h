3ec9f0efc1eff7980e660ecad9c49445ac5ae9e8
amanswarnakar <amanswarnakar2002@gmail.com>
2022-11-03 13:15:41 +0530

    Change the menu

--- headerfiles/phonebook.h
+++ headerfiles/phonebook.h
@@ -5,100 +5,100 @@ using namespace std;
 class TrieNode
 {
 public:
-    char data;
-    TrieNode *child[26];
-    bool isEnd;
+	char data;
+	TrieNode *child[26];
+	bool isEnd;
 
-    TrieNode(char ch)
-    {
-        data = ch;
-        for (int i = 0; i < 26; i++)
-        {
-            child[i] = NULL;
-        }
-        isEnd = false;
-    }
+	TrieNode(char ch)
+	{
+		data = ch;
+		for (int i = 0; i < 26; i++)
+		{
+			child[i] = NULL;
+		}
+		isEnd = false;
+	}
 };
 
 class Trie
 {
 
 public:
-    TrieNode *root;
+	TrieNode *root;
 
-    Trie()
-    {
-        root = new TrieNode('\0');
-    }
+	Trie()
+	{
+		root = new TrieNode('\0');
+	}
 
-    void insertUtility(TrieNode *root, string word)
-    {
-        if (word.length() == 0)
-        {
-            root->isEnd = true;
-            return;
-        }
+	void insertUtility(TrieNode *root, string word)
+	{
+		if (word.length() == 0)
+		{
+			root->isEnd = true;
+			return;
+		}
 
-        int index = word[0] - 'a';
-        TrieNode *child;
+		int index = word[0] - 'a';
+		TrieNode *child;
 
-        if (root->child[index] != NULL)
-        {
-            child = root->child[index];
-        }
-        else
-        {
-            child = new TrieNode(word[0]);
-            root->child[index] = child;
-        }
+		if (root->child[index] != NULL)
+		{
+			child = root->child[index];
+		}
+		else
+		{
+			child = new TrieNode(word[0]);
+			root->child[index] = child;
+		}
 
-        insertUtility(child, word.substr(1));
-    }
+		insertUtility(child, word.substr(1));
+	}
 
-    void insert(string word)
-    {
-        insertUtility(root, word);
-    }
+	void insert(string word)
+	{
+		insertUtility(root, word);
+	}
 
-    bool completeSearchUtility(TrieNode *root, string word)
-    {
-        if (word.length() == 0)
-        {
-            return root->isEnd;
-        }
+	bool completeSearchUtility(TrieNode *root, string word)
+	{
+		if (word.length() == 0)
+		{
+			return root->isEnd;
+		}
 
-        int index = word[0] - 'a';
-        TrieNode *child;
+		int index = word[0] - 'a';
+		TrieNode *child;
 
-        if (root->child[index] != NULL)
-        {
-            child = root->child[index];
-        }
-        else
-        {
-            return false;
-        }
+		if (root->child[index] != NULL)
+		{
+			child = root->child[index];
+		}
+		else
+		{
+			return false;
+		}
 
-        return completeSearchUtility(child, word.substr(1));
-    }
+		return completeSearchUtility(child, word.substr(1));
+	}
 
-    bool completeSearch(string word)
-    {
-        return completeSearchUtility(root, word);
-    }
+	bool completeSearch(string word)
+	{
+		return completeSearchUtility(root, word);
+	}
 };
 
 void addEntry(string insertVal, Trie obj)
 {
-    obj.insert(insertVal);
+	obj.insert(insertVal);
 }
 
-void searchPartially(string query)
+void searchPartially(string , Trie obj)
 {
-    cout << "Search Partially" << endl;
+	cout << "Search Partially" << endl;
 }
 
 bool searchCompletely(string query, Trie obj)
 {
-    return obj.completeSearch(query);
+	return obj.completeSearch(query);
 }
--- main.cpp
+++ main.cpp
@@ -4,54 +4,103 @@ using namespace std;
 
 int main()
 {
-    Trie fnameTrie, lnameTrie, phoneTrie;
-    while (true)
-    {
-        cout << "------------- Phonebook Application -------------\n";
-        cout << "Enter 1 to add\n";
-        cout << "Enter 2 to search\n";
-        cout << "Enter choice:\n";
-        int choice;
-        cin >> choice;
-        string fname, lname, phone;
-        string query;
+	Trie fnameTrie, lnameTrie, phoneTrie;
+	while (true)
+	{
+		cout << "------------- Phonebook Application -------------\n";
+		cout << "Enter 1 to add\n";
+		cout << "Enter 2 to search\n";
+		cout << "Enter choice:\n";
+		int choice, searchFilter;
+		cin >> choice;
+		string fname, lname, phone;
+		string query;
 
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter firstname:\n";
-            cin >> fname;
-            cout << "Enter lastname:\n";
-            cin >> lname;
-            cout << "Enter phone:\n";
-            cin >> phone;
-            addEntry(fname, fnameTrie);
-            addEntry(lname, lnameTrie);
-            addEntry(phone, phoneTrie);
-            break;
-        case 2:
-            cout << "Enter 1 to search partially\n";
-            cout << "Enter 2 to search completely\n";
-            int searchChoice;
-            cin >> searchChoice;
-            if (searchChoice == 1)
-            {
-                cout << "Enter query to search partially:\n";
-                cin >> query;
-                searchPartially(query);
-            }
-            else if (searchChoice == 2)
-            {
-                cout << "Enter query to search completely:\n";
-                cin >> query;
-                cout << boolalpha << searchCompletely(query, obj);
-            }
-            break;
+		switch (choice)
+		{
+		case 1:
+			cout << "Enter First Name:\n";
+			cin >> fname;
+			cout << "Enter Last Name:\n";
+			cin >> lname;
+			cout << "Enter Phone Number:\n";
+			cin >> phone;
+			addEntry(fname, fnameTrie);
+			addEntry(lname, lnameTrie);
+			addEntry(phone, phoneTrie);
+			break;
+		case 2:
+			while (true)
+			{
+				cout << "Enter 1 to search by First Name\n";
+				cout << "Enter 2 to search by Last Name\n";
+				cout << "Enter 3 to search by Phone Number\n";
+				cin >> searchFilter;
+				switch (searchFilter)
+				{
+				case 1:
+					cout << "Enter 1 to search partially by First Name\n";
+					cout << "Enter 2 to search completely by First Name\n";
+					int searchChoice;
+					cin >> searchChoice;
+					if (searchChoice == 1)
+					{
+						cout << "Enter query to search partially:\n";
+						cin >> query;
+						searchPartially(query, fnameTrie);
+					}
+					else if (searchChoice == 2)
+					{
+						cout << "Enter query to search completely:\n";
+						cin >> query;
+						cout << boolalpha << searchCompletely(query, fnameTrie);
+					}
+					break;
+				case 2:
+					cout << "Enter 1 to search partially by Last Name\n";
+					cout << "Enter 2 to search completely by Last Name\n";
+					int searchChoice;
+					cin >> searchChoice;
+					if (searchChoice == 1)
+					{
+						cout << "Enter query to search partially:\n";
+						cin >> query;
+						searchPartially(query, lnameTrie);
+					}
+					else if (searchChoice == 2)
+					{
+						cout << "Enter query to search completely:\n";
+						cin >> query;
+						cout << boolalpha << searchCompletely(query, lnameTrie);
+					}
+					break;
+				case 3:
+					cout << "Enter 1 to search partially by Phone Number\n";
+					cout << "Enter 2 to search completely by Phone Number\n";
+					int searchChoice;
+					cin >> searchChoice;
+					if (searchChoice == 1)
+					{
+						cout << "Enter the ";
+						cout << "Enter query to search partially:\n";
+						cin >> query;
+						searchPartially(query, phoneTrie);
+					}
+					else if (searchChoice == 2)
+					{
+						cout << "Enter query to search completely:\n";
+						cin >> query;
+						cout << boolalpha << searchCompletely(query, phoneTrie);
+					}
+					break;
+				}
+			}
+			break;
 
-        default:
-            cout << "Choice other than A, B and C";
-            break;
-        }
-    }
-    return 0;
+		default:
+			cout << "Choice other than A, B and C";
+			break;
+		}
+	}
+	return 0;
 }
