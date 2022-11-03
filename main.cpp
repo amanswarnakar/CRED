c6a26dc0c6b8b000aac88fc253a040ab456cfc4e
nBeauty72 <beautynirala.bn@gmail.com>
2022-11-03 11:22:55 +0530

    add menu

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "headerfiles/phonebook.h"
 using namespace std;
-
+ 
 
 int main()
 {
-  cout<<function('a');
-  
-  return 0;
+    string firstName= " ";
+    switch (x) {
+    case 'A':
+        cout << ;
+        break;
+    case 'B':
+        cout << " ";
+        break;
+    case 'C':
+        cout << "Choice is C";
+        break;
+    default:
+        cout << "Choice other than A, B and C";
+        break;
+    }
+    return 0;
 }
-
