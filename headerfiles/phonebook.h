69b096a8c3f9539c3f0f96a423afe3805a20fdb6
amanswarnakar <amanswarnakar2002@gmail.com>
2022-11-03 11:42:30 +0530

    Change main function

--- headerfiles/phonebook.h
+++ headerfiles/phonebook.h
@@ -1,6 +1,20 @@
+#include <bits/stdc++.h>
 #pragma once
 
-const char *function(char x)
+const char *func(char x)
 {
   return "abc";
+}
+void addEntry(string fname, string lname, string phone)
+{
+  cout << fname << " " << lname << " " << phone << endl;
+}
+
+void searchPartially(string query)
+{
+  cout << "Search Partially" << endl;
+}
+void searchCompletely(string query)
+{
+  cout << "Search Completely" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,56 @@
 #include <bits/stdc++.h>
 #include "headerfiles/phonebook.h"
 using namespace std;
- 
 
 int main()
 {
-    string firstName= " ";
-    switch (x) {
-    case 'A':
-        cout << ;
-        break;
-    case 'B':
-        cout << " ";
-        break;
-    case 'C':
-        cout << "Choice is C";
-        break;
+  func('a');
+  // string firstName = " ";
+  while (true)
+  {
+    cout << "------------- Phonebook Application -------------\n";
+    cout << "Enter 1 to add\n";
+    cout << "Enter 2 to search\n";
+    cout << "Enter choice";
+    int choice;
+    cin >> choice;
+    string fname, lname, phone;
+    string query;
+
+    switch (choice)
+    {
+    case 1:
+      cout << "Enter firstname:\n";
+      cin >> fname;
+      cout << "Enter lastname:\n";
+      cin >> lname;
+      cout << "Enter phone:\n";
+      cin >> phone;
+      addEntry(fname, lname, phone);
+      break;
+    case 2:
+      cout << "Enter 1 to search partially\n";
+      cout << "Enter 2 to search partially\n";
+      int searchChoice;
+      cin >> searchChoice;
+      if (searchChoice == 1)
+      {
+        cout << "Enter query to search partially:\n";
+        cin >> query;
+        searchPartially(query);
+      }
+      else if (searchChoice == 2)
+      {
+        cout << "Enter query to search completely:\n";
+        cin >> query;
+        searchCompletely(query);
+      }
+      break;
+
     default:
-        cout << "Choice other than A, B and C";
-        break;
+      cout << "Choice other than A, B and C";
+      break;
     }
-    return 0;
+  }
+  return 0;
 }
