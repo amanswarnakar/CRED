032144d72ae98a06de246dbcd1562f642d0cf349
Mac-Rose <phoenixstk17@gmail.com>
2022-11-04 09:09:22 +0530

    Update the searching feature in trie

--- headerfiles/phonebook.h
+++ headerfiles/phonebook.h
@@ -4,62 +4,224 @@ using namespace std;
 struct TrieNode
 {
 	struct TrieNode *children[26];
-	bool isWordEnd;
+	vector<int> isWordEnd;
 };
 
 struct TrieNode *constructor()
 {
 	struct TrieNode *parent = new TrieNode;
-	parent->isWordEnd = false;
 
 	for (int i = 0; i < 26; i++)
 		parent->children[i] = NULL;
 
 	return parent;
+};
+
+int SerialNo = 0;
+int welcomeMenu()
+{
+	cout << "\n------------- Phonebook Application ------------\n\n";
+	cout << "Enter 1 to add contact\n";
+	cout << "Enter 2 to search contact\n";
+	cout << "Enter any other number to exit\n";
+	int choice;
+	if (!(cin >> choice) or choice > 2)
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		choice = 0;
+	}
+	return choice;
 }
 
-void insert(struct TrieNode *root, string key)
+unordered_map<int, vector<string>> database;
+
+struct TrieNode *fnameTrie = constructor();
+struct TrieNode *lnameTrie = constructor();
+struct TrieNode *phoneTrie = constructor();
+
+void insertInTrie(struct TrieNode *root, string key, int SerialNo)
 {
 	struct TrieNode *iterator = root;
 
 	for (int level = 0; level < key.length(); level++)
 	{
 		int index = key[level] - 'a';
+		if (isdigit(key[level]))
+			index = key[level] - '0';
 		if (!iterator->children[index])
 			iterator->children[index] = constructor();
 
 		iterator = iterator->children[index];
 	}
 
-	iterator->isWordEnd = true;
+	iterator->isWordEnd.push_back(SerialNo);
 }
 
-void dfs(struct TrieNode *root, string currPrefix)
+bool isPhoneNumber(string &phone)
 {
-	if (root->isWordEnd)
-		cout << currPrefix << endl;
+	for (char &digit : phone)
+		if (!isdigit(digit))
+			return false;
+
+	return true;
+}
+
+void insertInPhoneBook()
+{
+	string fname, lname, phone;
+
+	cout << "Enter First Name:\n";
+	cin >> fname;
+	cout << "Enter Last Name:\n";
+	cin >> lname;
+
+	while (true)
+	{
+		cout << "Enter Phone Number:\n";
+		cin >> phone;
+
+		if (!isPhoneNumber(phone))
+			cout << "You have entered invalid phone number. Please try again.\n";
+		else
+		{
+			SerialNo++;
+			database.insert({SerialNo, {fname, lname, phone}});
+
+			transform(fname.begin(), fname.end(), fname.begin(), ::tolower);
+			transform(lname.begin(), lname.end(), lname.begin(), ::tolower);
+
+			insertInTrie(fnameTrie, fname, SerialNo);
+			insertInTrie(lnameTrie, lname, SerialNo);
+			insertInTrie(phoneTrie, phone, SerialNo);
+
+			break;
+		}
+	}
+}
+
+void printer(struct TrieNode *iterator)
+{
+	for (int &it : iterator->isWordEnd)
+	{
+		for (string &details : database[it])
+			cout << details << '\t';
+		cout << endl;
+	}
+}
+
+void DFS(struct TrieNode *root, string currPrefix)
+{
+	printer(root);
 
 	for (int i = 0; i < 26; i++)
 		if (root->children[i])
 		{
 			char child = 'a' + i;
-			dfs(root->children[i], currPrefix + child);
+			if (isdigit(currPrefix.back()))
+				child = '0' + i;
+			DFS(root->children[i], currPrefix + child);
 		}
 }
 
+void searchCompletely(TrieNode *root, string query)
+{
+	struct TrieNode *iterator = root;
+	for (char &c : query)
+	{
+		int ind = c - 'a';
+		if (isdigit(c))
+			ind = c - '0';
+		if (!iterator->children[ind])
+		{
+			cout << "No Complete Match for the query.\n";
+			return;
+		}
+
+		iterator = iterator->children[ind];
+	}
+
+	if (iterator->isWordEnd.size())
+	{
+		printer(iterator);
+	}
+	else
+		cout << "No Complete Match for the query.\n";
+}
 int searchPartially(TrieNode *root, string query)
 {
 	struct TrieNode *iterator = root;
 	for (char &c : query)
 	{
 		int ind = c - 'a';
+		if (isdigit(c))
+			ind = c - '0';
 		if (!iterator->children[ind])
 			return 0;
 
 		iterator = iterator->children[ind];
 	}
-
-	// complete matching wala start from here
-	dfs(iterator, query);
+	DFS(iterator, query);
 	return 1;
+}
+
+void searchInPhoneBook()
+{
+	cout << "Enter 1 to search by First Name\n";
+	cout << "Enter 2 to search by Last Name\n";
+	cout << "Enter 3 to search by Phone Number\n";
+
+	TrieNode *TrieHeader;
+	int searchFilter;
+	while (!(cin >> searchFilter and searchFilter < 4))
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid Input. Please re-enter.\n";
+	}
+	if (searchFilter == 1)
+	{
+		TrieHeader = fnameTrie;
+	}
+	else if (searchFilter == 2)
+	{
+		TrieHeader = lnameTrie;
+	}
+	else
+	{
+		TrieHeader = phoneTrie;
+	}
+
+	cout << "Enter 1 to search Partially\n";
+	cout << "Enter 2 to search Completely\n";
+	int searchChoice;
+	string query;
+
+	while (!(cin >> searchChoice) or searchChoice > 3)
+	{
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Invalid Input. Please re-enter.\n";
+	}
+
+	if (searchChoice == 1)
+	{
+		cout << "Enter query to search partially:\n";
+		cin >> query;
+		transform(query.begin(), query.end(), query.begin(), ::tolower);
+		cout << "\t\tOUTPUT\n";
+		int result = searchPartially(TrieHeader, query);
+		if (!result)
+		{
+			cout << "No Matching Result Found.\n";
+		}
+	}
+	else
+	{
+		cout << "Enter query to search completely:\t";
+		cin >> query;
+		transform(query.begin(), query.end(), query.begin(), ::tolower);
+		cout << "\t\tOUTPUT\n";
+		searchCompletely(TrieHeader, query);
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,126 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include "headerfiles/phonebook.h"
 using namespace std;
 
 int main()
 {
-	// Trie fnameTrie, lnameTrie, phoneTrie;
-	struct TrieNode *fnameTrie = constructor();
-	struct TrieNode *lnameTrie = constructor();
-	struct TrieNode *phoneTrie = constructor();
 	while (true)
 	{
-		cout << "\n------------- Phonebook Application ------------\n\n";
-		cout << "Enter 1 to add contact\n";
-		cout << "Enter 2 to search contact\n";
-		cout << "Enter any other number to exit\n";
-		cout << "Enter choice:\n";
-		int choice, searchChoice;
-		cin >> choice;
-		bool phoneFlag = true;
-		string fname, lname, phone;
-		string query;
-
+		int choice = welcomeMenu();
 		switch (choice)
 		{
 		case 1:
-			cout << "Enter First Name:\n";
-			cin >> fname;
-			cout << "Enter Last Name:\n";
-			cin >> lname;
-			while (true)
-			{
-				cout << "Enter Phone Number:\n";
-				cin >> phone;
-				phoneFlag = true;
-				if (phone.size() == 10)
-				{
-					for (auto &s : phone)
-					{
-						if (s - '0' > 9 or s - '0' < 0)
-						{
-							phoneFlag = false;
-							break;
-						}
-					}
-				}
-				else
-				{
-					phoneFlag = false;
-				}
-				if (!phoneFlag)
-					cout << "You have entered invalid phone number. Please try again.\n";
-				else
-				{
-					break;
-				}
-			}
-
-			insert(fnameTrie, fname);
-			insert(lnameTrie, lname);
-			insert(phoneTrie, phone);
-
+			insertInPhoneBook();
 			break;
 		case 2:
-			int searchFilter;
-			while (true)
-			{
-				cout << "Enter 1 to search by First Name\n";
-				cout << "Enter 2 to search by Last Name\n";
-				cout << "Enter 3 to search by Phone Number(10 Digits)\n";
-				cin >> searchFilter;
-				bool searchFlag = true;
-
-				if (searchFilter > 3)
-				{
-					searchFlag = false;
-				}
-				if (!searchFlag)
-					cout << "You have entered invalid choice. Please try again.\n";
-				else
-				{
-					break;
-				}
-			}
-
-			TrieNode *TrieHeader;
-			if (searchFilter == 1)
-				TrieHeader = fnameTrie;
-			else if (searchFilter == 2)
-				TrieHeader = lnameTrie;
-			else if (searchFilter == 3)
-				TrieHeader = phoneTrie;
-			else
-				cout << "Wrong Option Selected.\n";
-
-			cout << "Enter 1 to search Partially\n";
-			cout << "Enter 2 to search Completely\n";
-			cin >> searchChoice;
-			if (searchChoice == 1)
-			{
-				cout << "Enter query to search partially:\n";
-				cin >> query;
-				cout << "OUTPUT\n";
-				int result = searchPartially(TrieHeader, query);
-				if (!result)
-				{
-					cout << "No Matching Result Found.\n";
-				}
-			}
-			else if (searchChoice == 2)
-			{
-				// STILL TO BE DONE
-			}
-			else
-			{
-				cout << "Invalid Choice. PLEASE TRY AGAIN!\n";
-				break;
-			}
+			searchInPhoneBook();
 			break;
 		default:
 			cout << "Thank you for using PhoneBook Application.\n -> by Blackmar-Diemer Gambit\n";
-			return 0;
-			break;
+			exit(0);
 		}
 	}
 }
